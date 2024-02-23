4be5fd07b98595af3c349e0a03fa85ee3a6582c9
KamilBettin <bettinkamil@gmail.com>
2024-02-23 19:05:45 +0100

    +validering

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -2,18 +2,41 @@ const ticketForm = document.getElementById("ticketForm");
 const ticketList = document.getElementById("ticketList");
 const deleteTicketsBtn = document.getElementById("deleteTickets");
 
+
+
 const tickets = [];
+
 
 ticketForm.addEventListener("submit", function(event) {
     event.preventDefault();
 
-    const name = document.getElementById("name").value;
+    const name = document.getElementById("name").value.trim();
     const antall = document.getElementById("antall").value;
-    const surname = document.getElementById("surname").value;
+    const surname = document.getElementById("surname").value.trim();
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phone").value;
     const ticketType = document.getElementById("ticketType").value;
 
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const phonePattern = /^\d{8}$/;
+
+    if (isNaN(antall) || antall <=0 || antall >10) {
+        alert("Vennligst skriv antall billetter (min. 1 max 10)");
+        return false;
+    }
+
+    if (name === "" || surname === "") {
+        alert("Vennligst skriv for- og etternavnet ditt");
+        return false;
+    }
+    if (!emailPattern.test(email)) {
+        alert("Vennligst skriv korrekt epostadresse");
+        return false;
+    }
+    if (!phonePattern.test(phone)) {
+        alert("Vennligst skriv korrekt telefonnummer (8 sifre)");
+        return false;
+    }
 
     const ticket = {
         name: name,
